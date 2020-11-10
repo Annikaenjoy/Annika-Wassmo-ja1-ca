933035711f0939bed4ab1b608b1f6a228c1eebca
Annikaenjoy <69652654+Annikaenjoy@users.noreply.github.com>
2020-11-10 21:51:56 +0100

    Add files via upload

--- details.js
+++ details.js
@@ -32,9 +32,9 @@ function createHtml(details) {
     resultsContainer.innerHTML = `<div class="result">
             <h1>${details.name}</h1>
             <img src="${details.image}" alt="${details.name}" />
-            <div class="details.description">${details.gender}</div>
-            <div class="details.description">${details.species}</div>
-            <div class="details.description">${details.status}</div> 
+            <div class="details-description">Gender: ${details.gender}</div>
+            <div class="details-description">Species: ${details.species}</div>
+            <div class="details-description">Status: ${details.status}</div> 
                                   </div> `;
     document.title = `${details.name}`;
 
